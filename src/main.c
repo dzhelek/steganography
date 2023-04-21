a548379c44890f17bd1503e2a531cc92c22789b8
Yoan Dzhelekarski <yoan.dzhelekarski@gmail.com>
2023-04-21 21:57:51 +0300

    encode into image, decode from image

--- src/main.c
+++ src/main.c
@@ -6,13 +6,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BUFFER_SIZE 5
+
 typedef enum {
     NO_ERROR,
     ERR_OPEN,
     ERR_READ,
     ERR_WRITE,
+    ERR_ALLOC,
     ERR_UNSUPPORTED,
     ERR_ENCODE,
+    ERR_DECODE,
 } err_t;
 
 struct {
@@ -44,10 +48,16 @@ typedef struct {
     uint8_t reserved;
 } rgb_quad_t;
 
-err_t process(void* buffer, size_t size, FILE* input, FILE* output) {
+err_t process(void* buffer, size_t size, FILE* input, FILE* output, uint8_t expect_eof) {
     if (!fread(buffer, size, 1, input)) {
-        fprintf(stderr, "Error when reading input file!");
-        return ERR_READ;
+        if (!expect_eof && feof(input)) {
+            fprintf(stderr, "ERROR: Unexpected EOF");
+            return ERR_READ;
+        }
+        if (ferror(input)) {
+            fprintf(stderr, "Error when reading input file!");
+            return ERR_READ;
+        }
     }
     if (output != NULL && !fwrite(buffer, size, 1, output)) {
         fprintf(stderr, "Error when writing output file!");
@@ -61,19 +71,19 @@ err_t process_headers(FILE* input, FILE* output) {
     uint8_t* buffer, length;
 
     // process file header
-    err = process(&file_header.type, 2, input, output);
+    err = process(&file_header.type, 2, input, output, 0);
     if (err) return err;
     if (file_header.type != 0x4d42) {
         fprintf(stderr, "ERROR: unsupported file format");
         return ERR_UNSUPPORTED;
     }
-    err = process(&file_header.size, 4, input, output);
+    err = process(&file_header.size, 4, input, output, 0);
     if (err) return err;
-    err = process(&file_header.reserved1, 2, input, output);
+    err = process(&file_header.reserved1, 2, input, output, 0);
     if (err) return err;
-    err = process(&file_header.reserved2, 2, input, output);
+    err = process(&file_header.reserved2, 2, input, output, 0);
     if (err) return err;
-    err = process(&file_header.offset, 4, input, output);
+    err = process(&file_header.offset, 4, input, output, 0);
     if (err) return err;
     if (file_header.size - file_header.offset < 50) {
         fprintf(stderr, "ERROR: unable to encode message of such length in file of such size");
@@ -81,49 +91,51 @@ err_t process_headers(FILE* input, FILE* output) {
     }
 
     // process DIB
-    err = process(&info_header.size, 4, input, output);
+    err = process(&info_header.size, 4, input, output, 0);
     if (err) return err;
-    err = process(&info_header.width, 4, input, output);
+    err = process(&info_header.width, 4, input, output, 0);
     if (err) return err;
-    err = process(&info_header.height, 4, input, output);
+    err = process(&info_header.height, 4, input, output, 0);
     if (err) return err;
-    err = process(&info_header.planes, 2, input, output);
+    err = process(&info_header.planes, 2, input, output, 0);
     if (err) return err;
-    err = process(&info_header.bitCount, 2, input, output);
+    err = process(&info_header.bitCount, 2, input, output, 0);
     if (err) return err;
     if (info_header.bitCount != 24) {
         fprintf(stderr, "ERROR: %d-bit images are not supported", info_header.bitCount);
         return ERR_UNSUPPORTED;
     }
-    err = process(&info_header.compression, 4, input, output);
+    err = process(&info_header.compression, 4, input, output, 0);
     if (err) return err;
     if (info_header.compression) {
         fprintf(stderr, "ERROR: unsupported file compression");
         return ERR_UNSUPPORTED;
     }
-    err = process(&info_header.sizeImage, 4, input, output);
+    err = process(&info_header.sizeImage, 4, input, output, 0);
     if (err) return err;
-    err = process(&info_header.xPixelsPerMeter, 4, input, output);
+    err = process(&info_header.xPixelsPerMeter, 4, input, output, 0);
     if (err) return err;
-    err = process(&info_header.yPixelsPerMeter, 4, input, output);
+    err = process(&info_header.yPixelsPerMeter, 4, input, output, 0);
     if (err) return err;
-    err = process(&info_header.clrUsed, 4, input, output);
+    err = process(&info_header.clrUsed, 4, input, output, 0);
     if (err) return err;
-    err = process(&info_header.clrImportant, 4, input, output);
+    err = process(&info_header.clrImportant, 4, input, output, 0);
     if (err) return err;
 
     if ((length = file_header.offset - info_header.size - 14)) {
         buffer = calloc(length, 1);
-        err = process(buffer, length, input, output);
+        err = process(buffer, length, input, output, 0);
         if (err) return err;
     }
 
     return NO_ERROR;
 }
 
-void encode(void) {
+void encode(char* message) {
     FILE* input_file, * output_file;
     err_t err;
+    uint8_t carrier[8], bits, car_i;
+    uint64_t length, mes_i;
 
 //    input_file = fopen("pm_extruded_block.bmp", "rb");
     input_file = fopen("hristo.bmp", "rb");
@@ -138,7 +150,6 @@ void encode(void) {
         exit(ERR_OPEN);
     }
 
-//    printf("%d", sizeof(file_header));
     err = process_headers(input_file, output_file);
     if (err != NO_ERROR){
         printf("\nfiles closed");
@@ -147,20 +158,93 @@ void encode(void) {
         exit(err);
     }
 
-//    fread(buffer, 1, 10, input_file);
-//    printf("\nbits: %x bpp\n", info_header.bitCount);
-    printf("\nwidth: %x p\n", info_header.width);
-//    for (int i = 0; i < 10; i++) printf("%x ", buffer[i]);
+    fread(carrier, 1, 8, input_file);
+    for (bits = 0; bits < 8; bits++) {
+        carrier[bits] &= 0xFE;
+    }
+    fwrite(carrier, 1, 8, output_file);
+
+    length = strlen(message);
+    printf("\nlen: %d\n", length);
+    for (mes_i = 0; mes_i <= length; mes_i++) {
+        fread(carrier, 1, 8, input_file);
+        for (bits = 0; bits < 8; bits++) {
+            if ((message[mes_i] >> bits) & 1) {
+                carrier[bits] |= 1;
+            }
+            else {
+                carrier[bits] &= 0xFE;
+            }
+        }
+        fwrite(carrier, 1, 8, output_file);
+    }
+
+    while (!feof(input_file)) {
+        process(&bits, 1, input_file, output_file, 1);
+    }
 
     fclose(input_file);
     fclose(output_file);
 
 }
 
+unsigned char* decode() {
+    FILE* input_file, * output_file;
+    err_t err;
+    uint8_t carrier[8], bits, car_i;
+    uint64_t length, mes_i;
+    uint8_t* message = calloc(BUFFER_SIZE, 1);
+
+    if (message == NULL) {
+        fprintf(stderr, "ERROR: not enough dynamic memory");
+        exit(ERR_ALLOC);
+    }
+
+//    input_file = fopen("pm_extruded_block.bmp", "rb");
+    input_file = fopen("output.bmp", "rb");
+    if (input_file == NULL) {
+        fprintf(stderr, "Error when opening input file!");
+        exit(ERR_OPEN);
+    }
+
+    err = process_headers(input_file, NULL);
+
+    fread(carrier, 1, 8, input_file);
+    for (bits = 0; bits < 8; bits++) {
+        if (carrier[bits] & 1) {
+            fprintf(stderr, "Error when decoding message");
+            exit(ERR_DECODE);
+        }
+    }
+
+    mes_i = 0;
+    do {
+        if (mes_i == BUFFER_SIZE) {
+            message = realloc(message, mes_i + BUFFER_SIZE);
+        }
+        message[mes_i] = 0;
+        fread(carrier, 1, 8, input_file);
+        for (bits = 0; bits < 8; bits++) {
+            if (carrier[bits] & 1) {
+                message[mes_i] |= 1 << bits;
+            }
+        }
+    } while(message[mes_i++]);
+
+    fclose(input_file);
+
+    return message;
+}
+
 int main(void) {
-//    encode();
-    char message[] = "Hello World!";
-    uint8_t carrier[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, };
+//    char message[] = "Hello World!";
+//    encode(message);
+    unsigned char* buffer;
+    buffer = decode();
+    printf("\n%s", buffer);
+    /*
+//    uint8_t carrier[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, };
+    uint8_t carrier[] = {0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1, 0x00, 0xFF, 0xAA, 0x1};
     uint8_t mask;
 
     int mes_i, car_i, bits;
@@ -168,15 +252,6 @@ int main(void) {
     for (car_i = 0; car_i < 8; car_i++) {
         carrier[car_i] &= 0xFE;
     }
-
-    /*
-        carrier[car_i]                                     // 1111 1111
-        message[mes_i]                                     // 0100 0010
-
-        message[mes_i] >> bits                             // 0010 0001
-        (message[mes_i] >> bits) & 1                       // 0000 0001
-        (message[mes_i] >> bits) | 0xFE                    // 1111 1111
-     */
 
     for (mes_i = 0; mes_i <= length; mes_i++) {
         for (bits = 0; bits < 8; bits++) {
@@ -206,6 +281,7 @@ int main(void) {
     } while(decoded_message[mes_i++]);
     printf("\n%s", decoded_message);
     printf("\n%d", mes_i);
+     */
 
     return 0;
 }
